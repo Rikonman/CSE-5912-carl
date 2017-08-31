280b8939aa7d58d4d004c0e4055c0d78c3e2ac2b
Ryan Stutzman <rstutz2408@gmail.com>
2017-08-30 23:06:49 -0400

    Removed the extreme 3D version that I made since the original game is now getting unflattened.

--- Assets/MenuScene/Scripts/ButtonManager.cs
+++ Assets/MenuScene/Scripts/ButtonManager.cs
@@ -5,14 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    public void btnNewGame2D_OnClick()
+    public void btnNewGame_OnClick()
     {
         SceneManager.LoadScene("pong");
-    }
-
-    public void btnNewGame3D_OnClick()
-    {
-        SceneManager.LoadScene("Pong3D");
     }
 
     public void btnExitGame_OnClick()
