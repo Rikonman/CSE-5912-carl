75aad448d02f9c1850cfd030bb9b71ed8f8b918c
Ryan Stutzman <rstutz2408@gmail.com>
2017-08-30 21:11:23 -0400

    Added a 3D environment with a material as well as a menu leading into the game

---
+++ Assets/MenuScene/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void btnNewGame2D_OnClick()
+    {
+        SceneManager.LoadScene("pong");
+    }
+
+    public void btnNewGame3D_OnClick()
+    {
+        SceneManager.LoadScene("Pong3D");
+    }
+
+    public void btnExitGame_OnClick()
+    {
+        Application.Quit();
+    }
+}
