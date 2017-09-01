cc71225865739ea13904decafa7d3551b658c0a7
Ryan Stutzman <rstutz2408@gmail.com>
2017-09-01 19:48:35 -0400

    For some reason things didn't get committed last time so this is to make sure everything gets pushed.

---
+++ Assets/Game/Scripts/MainCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+    public delegate void GameStartAnimCompleted();
+    public static event GameStartAnimCompleted gameStartAnimCompleted;
+
+    void CamGameStartAnimCompleted()
+    {
+        gameStartAnimCompleted();
+    }
+}
---
+++ Assets/Game/Scripts/NewGameCountdown.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewGameCountdown : MonoBehaviour {
+
+    public Text countdownText;
+    public int initialWaitTime = 5;
+
+    private DateTime timeNewGameStarted;
+
+    void Start()
+    {
+        countdownText.text = "";
+        timeNewGameStarted = DateTime.MinValue;
+        MainCamera.gameStartAnimCompleted += StartCountdown;
+        PauseMenu.ExitGame += ExitingGame;
+    }
+
+    void ExitingGame()
+    {
+        MainCamera.gameStartAnimCompleted -= StartCountdown;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (timeNewGameStarted == DateTime.MinValue)
+            return;
+
+        int timeElapsed = (int)DateTime.Now.Subtract(timeNewGameStarted).TotalSeconds;
+        int timeLeft = initialWaitTime - timeElapsed;
+        if (timeLeft > 0 && timeLeft.ToString() != countdownText.text)
+        {
+            countdownText.text = timeLeft.ToString();
+        }
+        if (timeLeft == 0)
+        {
+            PauseMenu.IsPaused = false;
+            PauseMenu.IsGameReady = true;
+            countdownText.text = "GO!";
+        }
+        else if (timeLeft == -1)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    void StartCountdown()
+    {
+        timeNewGameStarted = DateTime.Now;
+    }
+
+}
