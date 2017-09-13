e3525efd77fab038e5efe98ac4b1c4d08587c8d8
Bsokevitz <fuksyr@gmail.com>
2017-09-13 11:26:38 -0400

    damage script added

--- Assets/Game/Scripts/Gun.cs
+++ Assets/Game/Scripts/Gun.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour {
 
-    public float damage = 10f;
+    public float damage = 1f;
     public float range = 100f;
     public Camera fpsCamera;
 
@@ -24,6 +24,11 @@ public class Gun : MonoBehaviour {
         RaycastHit hit;
         if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range)) {
             Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null) {
+                target.TakeDamage(damage);
+            }
         }
     }
 }
---
+++ Assets/Game/Scripts/Target.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    public float health = 5f;
+
+    public void TakeDamage(float damage) {
+        health -= damage;
+
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
