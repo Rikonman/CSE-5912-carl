037c5479fdfa2291d515b11822f14fe8712d7856
Bsokevitz <fuksyr@gmail.com>
2017-09-12 21:57:45 -0400

    some basic fps stuff
    basic fps stuff

---
+++ Assets/Game/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    public float lookSensitivity = 5f;
+    float xRotation;
+    float yRotation;
+    float xRotationV;
+    float yRotationV;
+    float lookSmoothDamp = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f, 0, Input.GetAxis("Vertical") * Time.deltaTime * 3.0f);
+
+    }
+}
