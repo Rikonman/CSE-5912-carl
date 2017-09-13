6bb3c222151505456b7ed731ec6ea7454708be63
Contextual <igetspammed1234@gmail.com>
2017-09-13 01:07:10 -0400

    Initial Networking
    Two Players can connect (Ctrl+B to build and run one in editor)
    Basic lobby
    Basic movement shared
    Guns seem to not load though

---
+++ Assets/SpawnController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnController : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] disabled;
+
+	Camera LobbyCam; 
+
+	void Start()
+	{
+		if (!isLocalPlayer) {
+			for (int i = 0; i < disabled.Length; i++) {
+				disabled [i].enabled = false; 
+			}
+		} else {
+			LobbyCam = Camera.main;
+			if(LobbyCam != null)
+				LobbyCam.gameObject.SetActive (false);
+		}
+	}
+		
+	void OnDisable()
+	{
+		if (LobbyCam != null)
+			LobbyCam.gameObject.SetActive (true);
+	}
+
+}
