14a72735a9d10a7d68e2859713c5438239737cca
Bsokevitz <fuksyr@gmail.com>
2017-09-13 15:19:02 -0400

    added force to the bullets

--- Assets/Game/Scripts/Gun.cs
+++ Assets/Game/Scripts/Gun.cs
@@ -9,6 +9,10 @@ public class Gun : MonoBehaviour {
     public Camera fpsCamera;
     public ParticleSystem flash;
     public GameObject hitEffect;
+    public float force = 60f;
+    public float fireRate = .1f;
+    public float fireDelay = 0f;
+    public bool automatic = false;
 
     // Use this for initialization
     void Start () {
@@ -17,8 +21,18 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire1")) {
-            Shoot();
+        if (automatic) {
+            if (Input.GetButton("Fire1") && Time.time >= fireDelay)
+            {
+                fireDelay = Time.time + 1f / fireRate;
+                Shoot();
+            }
+        } else {
+            if (Input.GetButtonDown("Fire1") && Time.time >= fireDelay)
+            {
+                fireDelay = Time.time + fireRate;
+                Shoot();
+            }
         }
 	}
 
@@ -31,6 +45,11 @@ public class Gun : MonoBehaviour {
             Target target = hit.transform.GetComponent<Target>();
             if (target != null) {
                 target.TakeDamage(damage);
+            }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * force);
             }
 
             GameObject tempHitEffect = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
