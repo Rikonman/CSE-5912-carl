5b28735e9ca39b6734d3cf0069693cb9cadcd363
Contextual <igetspammed1234@gmail.com>
2017-09-14 16:01:06 -0400

    Added "jumping" w/ space bar

--- Assets/Game/Scripts/PlayerControls.cs
+++ Assets/Game/Scripts/PlayerControls.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))] 
 public class PlayerControls : MonoBehaviour {
 
-    public float lookSensitivity = 5f;
+	[SerializeField]
+	private float jumpSensitivity = 1000f;
+	private float lookSensitivity = 5f;
+
     float xRotation;
     float yRotation;
     float xRotationV;
     float yRotationV;
     float lookSmoothDamp = 0.1f;
 
+	private Rigidbody rb; 
+
     // Use this for initialization
     void Start () {
-
+		rb = GetComponent<Rigidbody> (); 
     }
 	
 	// Update is called once per frame
@@ -24,5 +30,14 @@ public class PlayerControls : MonoBehaviour {
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f, 0, Input.GetAxis("Vertical") * Time.deltaTime * 3.0f);
 
+		Vector3 jumpForce = Vector3.zero; 
+
+		if (Input.GetButton ("Jump")) {
+			jumpForce = Vector3.up * jumpSensitivity; 
+			if (jumpForce != Vector3.zero)
+			{
+				rb.AddForce (Time.fixedDeltaTime * jumpForce, ForceMode.Acceleration); 
+			}
+		}
     }
 }
