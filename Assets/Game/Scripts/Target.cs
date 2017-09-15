2c2e3526d16b87dfe02896915dacc75c45da7877
Contextual <igetspammed1234@gmail.com>
2017-09-14 23:36:00 -0400

    Added invulnerability

--- Assets/Game/Scripts/Target.cs
+++ Assets/Game/Scripts/Target.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Target : MonoBehaviour {
 
     public float health = 50f;
+	public bool isVulnerable = true; 
 
     public void TakeDamage(float damage) {
-        health -= damage;
 
-        if (health <= 0) {
-            Destroy(gameObject);
-        }
+		if (isVulnerable) {
+			health -= damage;
+
+			if (health <= 0) {
+				Destroy (gameObject);
+			}
+		} else {
+			Debug.Log ("This object is invulnerable"); 
+		}
     }
 }
