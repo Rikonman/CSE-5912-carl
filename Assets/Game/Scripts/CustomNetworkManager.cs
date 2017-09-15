bd827b6a7aba6299b1215bc8c5d5feb093854e8f
Ryan Stutzman <rstutz2408@gmail.com>
2017-09-15 05:03:47 -0400

    Enabled basic networked shooting functionality
    Networking shooting is now enabled.

---
+++ Assets/Game/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager {
+
+    [Header("Scene Camera Properties")]
+    [SerializeField] Transform sceneCamera;
+    [SerializeField] float cameraRotationRadius = 100f;
+    [SerializeField] float cameraRotationSpeed = 10f;
+    [SerializeField] bool canRotate;
+
+    float rotation;
+
+    public override void OnStartClient(NetworkClient client)
+    {
+        base.OnStartClient(client);
+        canRotate = false;
+    }
+
+    public override void OnStartHost()
+    {
+        base.OnStartHost();
+        canRotate = false;
+    }
+
+    public override void OnStopClient()
+    {
+        base.OnStopClient();
+        canRotate = true;
+    }
+
+    public override void OnStopHost()
+    {
+        base.OnStopHost();
+        canRotate = true;
+    }
+
+    void Update()
+    {
+        if (!canRotate)
+            return;
+
+        rotation += cameraRotationSpeed * Time.deltaTime;
+        if (rotation >= 360f)
+            rotation -= 360f;
+
+        sceneCamera.position = Vector3.zero;
+        sceneCamera.rotation = Quaternion.Euler(0f, rotation, 0f);
+        sceneCamera.Translate(0f, cameraRotationRadius*0.75f, -cameraRotationRadius);
+        sceneCamera.LookAt(Vector3.zero);
+    }
+}
---
+++ Assets/Game/Scripts/GunController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GunController : NetworkBehaviour {
+
+    public float damage = 1f;
+    public float range = 100f;
+    public Camera fpsCamera;
+    public ParticleSystem flash;
+    public GameObject hitEffect;
+    public float force = 60f;
+    public float fireRate = .1f;
+    public float fireDelay = 0f;
+    public bool automatic = false;
+
+    [SerializeField]
+    GameObject projectilePrefab;
+    [SerializeField]
+    Transform barrellExit;
+
+    // the reset method lets us run slow code (like "Find") in the editor where performance
+    // won't impact the players at runtime
+    void Reset()
+    {
+        barrellExit = transform.Find("BarrellExit");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Only the local player can fire a weapon
+        if (!isLocalPlayer)
+            return;
+
+        if (automatic) {
+            if (Input.GetButton("Fire1") && Time.time >= fireDelay)
+            {
+                fireDelay = Time.time + 1f / fireRate;
+                Shoot();
+            }
+        } else {
+            if (Input.GetButtonDown("Fire1") && Time.time >= fireDelay)
+            {
+                fireDelay = Time.time + fireRate;
+                Shoot();
+            }
+        }
+	}
+
+    void Shoot() {
+        flash.Play();
+        CmdSpawnProjectile();
+        /*RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * force);
+            }
+
+            GameObject tempHitEffect = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(tempHitEffect, 0.3f);
+        }*/
+    }
+
+    // This command is called from the localPlayer and run on the server. Note that Commands must begin with 'Cmd'
+    [Command]
+    void CmdSpawnProjectile()
+    {
+        GameObject instance = Instantiate(projectilePrefab, barrellExit.position, barrellExit.rotation);
+        instance.GetComponent<Rigidbody>().AddForce(barrellExit.forward * range);
+
+        NetworkServer.Spawn(instance);
+    }
+}
