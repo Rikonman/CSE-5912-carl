05e3de9fe740c39882267710af3db71152a19c25
Alex Barnes <barnes.778@osu.edu>
2017-09-17 12:44:33 -0400

    Added mouse hide+lock script to Player prefab
    - When you enter game, mouse cursor is hidden and locked (so you don't
    have your cursor running around the editor while trying to test)
    - Press escape and move mouse to have cursor show up again in the
    editor.
    - I just added this bc I find it useful, disable the script on the
    Player prefab if you don't want to use it.

---
+++ Assets/Game/Scripts/MouseHideAndLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHideAndLock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = (false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = (false);
+    }
+}
