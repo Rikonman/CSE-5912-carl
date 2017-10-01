26efe2f9a953bf3ccd61bf0f3b4c8375bcd3ea65
Jake <ekajhtur@gmail.com>
2017-10-01 10:26:21 -0400

    Started work on console

---
+++ Assets/Game/SceneGame/Scripts/HostConsole.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+
+public class HostConsole : MonoBehaviour
+{
+    bool open;
+    GameObject panel;
+    InputField input;
+    void Start()
+    {
+        panel = GameObject.Find("Console");
+        input = panel.GetComponentInChildren<InputField>();
+        panel.SetActive(open);
+    }
+
+    void Update()
+    {
+        //Open close console
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            open = !open;
+            panel.SetActive(open);
+            if (open)
+            {
+                input.ActivateInputField();
+            }
+            else
+            {
+                input.text = "";
+            }
+        }
+        if (open)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                switch (input.text)
+                {
+                    case "":
+                        //Do something
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+
+
+    }
+}
