a1d1674b37abbf49d5f56722e5f72716eb560632
Bsokevitz <fuksyr@gmail.com>
2017-10-03 14:10:08 -0400

    some animation stuff added to main

---
+++ Assets/pistol animations/playerAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimation : MonoBehaviour {
+
+    public Animator anim;
+
+    private float inputH;
+    private float inputV;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("1")) {
+            anim.Play("HM_Aim_Revolver_Walk");
+        }
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+    }
+}
