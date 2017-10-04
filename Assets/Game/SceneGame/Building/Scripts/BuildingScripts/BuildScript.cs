99674f85f1848db57063d915580e199a89774c1d
Jake <ekajhtur@gmail.com>
2017-10-04 13:35:58 -0400

    Worked on building material indicator

--- Assets/Game/SceneGame/Building/Scripts/BuildingScripts/BuildScript.cs
+++ Assets/Game/SceneGame/Building/Scripts/BuildingScripts/BuildScript.cs
@@ -9,7 +9,7 @@ public class BuildScript : NetworkBehaviour
     //Fix placement rotations to be type independent
     //Clean up code (Getcomponents) and overall structure
     //Snapped and move away and click causes mount point to be removed
-
+    public List<Material> materials = new List<Material>();
 
     public int teamID;
     public bool buildMode;
@@ -24,7 +24,7 @@ public class BuildScript : NetworkBehaviour
     BuildPoints previewBuildPoints;
     MeshRenderer meshRend;
     int currentObject;
-    Material originalMaterial;    
+    Material originalMaterial;
     List<List<bool>> pointUsed = new List<List<bool>>();
     List<Vector3> placedObjects = new List<Vector3>();
 
@@ -38,7 +38,7 @@ public class BuildScript : NetworkBehaviour
     {
         camera = transform.GetChild(0);
         currentObject = 0;
-        if(teamID == 0)
+        if (teamID == 0)
         {
             baseParent = GameObject.Find("Base1Center").transform;
         }
@@ -80,7 +80,7 @@ public class BuildScript : NetworkBehaviour
     {
         Ray previewRay = new Ray(camera.position, camera.forward);
         RaycastHit previewHit;
-        //meshRend.material = invalidMaterial;
+        SetMaterial(invalidMaterial);
         previewObject.transform.eulerAngles = new Vector3(0, camera.eulerAngles.y - 90, 0);
         previewBuildPoints.valid = false;
         if (Physics.Raycast(previewRay, out previewHit, 5f))
@@ -89,7 +89,7 @@ public class BuildScript : NetworkBehaviour
 
             if (currentObject == 0 && previewHit.collider.gameObject.tag == "Ground")
             {
-                //meshRend.material = validMaterial;
+                SetMaterial(validMaterial);
                 previewBuildPoints.valid = true;
             }
 
@@ -109,23 +109,23 @@ public class BuildScript : NetworkBehaviour
     {
         foreach (MountPoint mp in mountPoints)
         {
-            for (int j = 0; j <  mp.pointType.Count; j++)
+            for (int j = 0; j < mp.pointType.Count; j++)
             {
                 if (mp.pointType[j] == previewBuildPoints.type)
                 {
                     objectIndex = mountPoints.IndexOf(mp);
                     vertexIndex = j;
                     Vector3 v3 = mp.points[vertexIndex];
-                    if (!pointUsed[objectIndex][vertexIndex])   
+                    if (!pointUsed[objectIndex][vertexIndex])
                     {
                         Vector3 newVec3 = mp.parent.TransformPoint(new Vector3(v3.x / (mp.parent.transform.localScale.x), v3.y / (mp.parent.transform.localScale.y), v3.z / (mp.parent.transform.localScale.z)));
                         Debug.DrawRay(newVec3, Vector3.up, Color.red);
                         if (Vector3.Distance(previewObject.transform.position, newVec3) < 2)
                         {
                             bool near = false;
-                            foreach(Vector3 nearV3 in placedObjects)
+                            foreach (Vector3 nearV3 in placedObjects)
                             {
-                                if(Vector3.Distance(nearV3,newVec3) < 0.1f)
+                                if (Vector3.Distance(nearV3, newVec3) < 0.1f)
                                 {
                                     near = true;
                                 }
@@ -139,7 +139,7 @@ public class BuildScript : NetworkBehaviour
                                 {
                                     previewObject.transform.localEulerAngles = mp.parent.transform.localEulerAngles;
                                 }
-                                if (previewBuildPoints.type == BuildPoints.MountType.Wall 
+                                if (previewBuildPoints.type == BuildPoints.MountType.Wall
                                     && mp.parent.GetComponent<BuildPoints>().type == BuildPoints.MountType.Door1)
                                 {
                                     previewObject.transform.localEulerAngles = mp.parent.transform.localEulerAngles;
@@ -149,7 +149,7 @@ public class BuildScript : NetworkBehaviour
                                 {
                                     previewObject.transform.localEulerAngles = mp.parent.transform.localEulerAngles;
                                 }
-                                //meshRend.material = validMaterial;
+                                SetMaterial(validMaterial);
                                 previewBuildPoints.valid = true;
 
                                 finalObjectIndex = mountPoints.IndexOf(mp);
@@ -170,12 +170,12 @@ public class BuildScript : NetworkBehaviour
         }
         currentObject = id;
         previewObject = Instantiate(objects[currentObject], baseParent);
-        //meshRend = previewObject.GetComponent<MeshRenderer>();
-        //originalMaterial = meshRend.material;
-        //meshRend.material = invalidMaterial;
+        meshRend = previewObject.GetComponent<MeshRenderer>();
+        SetMaterial(invalidMaterial);
         previewObject.layer = 2;
         previewBuildPoints = previewObject.GetComponent<BuildPoints>();
         previewObject.transform.localEulerAngles = new Vector3(0, 0, 0);
+        SaveMaterials();
     }
 
     void PlaceObject()
@@ -196,15 +196,61 @@ public class BuildScript : NetworkBehaviour
                 newBools.Add(used);
             }
             pointUsed.Add(newBools);
-            //meshRend.material = originalMaterial;
+            ResetMaterials();
             placedObjects.Add(previewObject.transform.position - previewBuildPoints.offset);
             previewObject.layer = 0;
-            Debug.Log("Placing " + objects[currentObject].ToString()+ "With Object ID:" + currentObject + " at X:" + previewObject.transform.position.x + "; Y:" + previewObject.transform.position.y + "; Z:" + previewObject.transform.position.z);
+            Debug.Log("Placing " + objects[currentObject].ToString() + "With Object ID:" + currentObject + " at X:" + previewObject.transform.position.x + "; Y:" + previewObject.transform.position.y + "; Z:" + previewObject.transform.position.z);
             CmdSpawnBuildingPart(objects[currentObject].ToString(), currentObject, previewObject.transform.position, previewObject.transform.rotation);
             previewObject = null;
-            //meshRend = null;
+            meshRend = null;
         }
     }
+
+    void SaveMaterials()
+    {
+        if(meshRend != null)
+        materials.Add(meshRend.material);
+        for (int i = 0; i < previewObject.transform.childCount; i++)
+        {
+            MeshRenderer rend = previewObject.transform.GetChild(i).GetComponent<MeshRenderer>();
+            if (rend != null)
+                materials.Add(rend.material);
+        }
+
+    }
+    void ResetMaterials()
+    {
+        int index = 0;
+        if (meshRend != null)
+        {
+            meshRend.material = materials[index];
+        }
+        else
+        {
+            index--;
+        }
+        for (int i = 0; i < previewObject.transform.childCount; i++)
+        {
+            MeshRenderer rend = previewObject.transform.GetChild(i).GetComponent<MeshRenderer>();
+            if (rend != null)
+            {
+                rend.material = materials[index + 1];
+                index++;
+            }
+        }
+    }
+    void SetMaterial(Material mat)
+    {
+        if(meshRend != null)
+        meshRend.material = mat;
+        for (int i = 0; i < previewObject.transform.childCount; i++)
+        {
+            MeshRenderer rend = previewObject.transform.GetChild(i).GetComponent<MeshRenderer>();
+            if (rend != null)
+                rend.material = mat;
+        }
+    }
+
     [Command]
     void CmdSpawnBuildingPart(string objectString, int objectID, Vector3 position, Quaternion rotation)
     {
