efc745a9890fea90a27c1d6542286a7e480b734d
Bsokevitz <fuksyr@gmail.com>
2017-10-04 17:55:21 -0400

    small camera fix

--- Assets/Game/SceneGame/Scripts/PlayerController.cs
+++ Assets/Game/SceneGame/Scripts/PlayerController.cs
@@ -81,7 +81,7 @@ public class PlayerController : NetworkBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         if (!locked)
         {
